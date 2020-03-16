7c391935cb17eaaaeee97c6f7f8b9cb189dc119a
felixleo22 <leofelixoff@outlook.fr>
2020-03-16 14:32:34 +0100

    ok

--- api/apiPlayer/server.js
+++ api/apiPlayer/server.js
@@ -1,6 +1,24 @@
 const express = require('express');
+const jwt = require('jsonwebtoken');
+
 // create app
 const app = express();
+
+/**
+ * données :
+ * - pseudo
+ * - série
+ * - niveau (éventuel)
+ * 
+ * @return 
+ * id de la partie
+ * token de la partie
+ */
+app.post("/start", (req, res) => {
+    //TODO filtrer les données
+    let param = req.query.params;
+    
+});
 
 app.listen(8082, () => {
     console.log('api player is running !');
