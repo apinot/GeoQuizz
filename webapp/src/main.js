7349fe4b64980491965a6427f31425e08cad9bca
apinot <antoine.pinot1@gmail.com>
2020-03-16 14:48:24 +0100

    axios

--- webapp/src/main.js
+++ webapp/src/main.js
@@ -3,7 +3,16 @@ import App from './App.vue';
 import router from './router';
 import store from './store';
 
+import axios from 'axios';
+
 Vue.config.productionTip = false;
+
+/* Configuratio d'axios */
+// Nom de domaine et port de l'api Player
+const apiHost = 'http://55ad5e57.ngrok.io/';
+axios.defaults.baseURL = apiHost;
+
+Vue.prototype.$http = Axios;
 
 new Vue({
   router,
