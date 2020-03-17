bbf768b16e4c8d82d752b8e631fac6b71d6adc72
felixleo22 <leofelixoff@outlook.fr>
2020-03-17 11:00:27 +0100

    router

--- webapp/src/App.vue
+++ webapp/src/App.vue
@@ -1,48 +1,21 @@
 
 <template>
   <div id="app">
-    <div class="container">
-      <div class="row">
-        <div class="input-field col s12">
-          <input v-model="userName" id="first_name" type="text" class="validate" />
-          <label for="first_name">Pseudo</label>
-        </div>
-      </div>
-      <button
-        v-on:click="createPartie"
-        class="btn waves-effect waves-light"
-        type="submit"
-        name="action">
-        Submit
-        <i class="material-icons right">send</i>
-      </button>
-    </div>
     <router-view />
   </div>
 </template>
 
 
 <script>
+
 export default {
   data() {
-    return {
-      userName: '',
-      data: '',
-    };
+    return {};
   },
 
-  methods: {
-    createPartie() {
-      console.log('coucou');
-      this.$http
-        .post('/parties', { username: this.userName })
-        .then((response) => {
-          this.data = response.data;
-        })
-        .catch((error) => {
-          console.log(error);
-        });
-    },
+  created() {
+    this.$store.dispatch('retrieveToken', response.data);
+    console.log(`a is: ${this.a}`);
   },
 };
 </script>
--- webapp/src/store/index.js
+++ webapp/src/store/index.js
@@ -5,10 +5,50 @@ Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
+    token: localStorage.getItem('token') || null,
+    partie: localStorage.getItem('partie') || null,
+  },
+  getters: {
+    getToken(state) {
+      return !!state.token;
+    },
+    getPartie(state) {
+      return state.partie;
+    },
   },
   mutations: {
+    retrieveToken(state, token, partie) {
+      state.token = token;
+      state.partie = partie;
+    },
+    destroyToken(state) {
+      state.token = null;
+      state.partie = null;
+    },
   },
   actions: {
+    retrieveToken(context, credentials) {
+      return new Promise((resolve) => {
+        const { token, partie } = credentials;
+        localStorage.setItem('token', token);
+        localStorage.setItem('partie', partie);
+        context.commit('retrieveToken', token, partie);
+        resolve();
+      }).catch((error) => {
+        console.log(error);
+      });
+    },
+
+    destroyToken(context) {
+      return new Promise((resolve) => {
+        if (context.getters.loggedIn) {
+          localStorage.removeItem('token');
+          localStorage.removeItem('partie');
+          context.commit('destroyToken');
+          resolve();
+        }
+      });
+    },
   },
   modules: {
   },
