331941c74cae90d4b2cd2934e74f312721ceab14
simon <simon.dal-ponte8@etu.univ-lorraine.fr>
2020-03-17 17:07:42 +0100

    ajout de l'upload pour le mobile pour la prise de photo avec localisation

--- api/apiMobile/server.js
+++ api/apiMobile/server.js
@@ -1,7 +1,14 @@
 const express = require('express');
 // create app
 const app = express();
+const express = require('express');
 
+// connexion à la base de donnée mongo
+mongoose.connect("mongodb://databaseGeoQuizz/Geoquizz", {
+    useCreateIndex: true,
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
 app.listen(8081, () => {
     console.log('api mobile is running !');
 });
