c6c7178189d9dcfc8d7f8ac98ea512590015bd38
felixleo22 <leofelixoff@outlook.fr>
2020-03-17 09:02:34 +0100

    try to fix cors

--- webapp/src/App.vue
+++ webapp/src/App.vue
@@ -1,3 +1,4 @@
+
 <template>
   <div id="app">
     <div class="container">
@@ -7,7 +8,11 @@
           <label for="first_name">Pseudo</label>
         </div>
       </div>
-      <button class="btn waves-effect waves-light" type="submit" name="action">
+      <button
+        v-on:click="createPartie"
+        class="btn waves-effect waves-light"
+        type="submit"
+        name="action">
         Submit
         <i class="material-icons right">send</i>
       </button>
@@ -22,7 +27,22 @@ export default {
   data() {
     return {
       userName: '',
+      data: '',
     };
+  },
+
+  methods: {
+    createPartie() {
+      console.log('coucou');
+      this.$http
+        .post('/parties', { username: this.userName })
+        .then((response) => {
+          this.data = response.data;
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    },
   },
 };
 </script>
--- webapp/src/main.js
+++ webapp/src/main.js
@@ -1,3 +1,4 @@
+
 import Vue from 'vue';
 import axios from 'axios';
 import App from './App.vue';
@@ -12,6 +13,9 @@ Vue.config.productionTip = false;
 const apiHost = 'http://55ad5e57.ngrok.io/';
 axios.defaults.baseURL = apiHost;
 
+axios.defaults.headers.common['Access-Control-Allow-Origin'] = '*';
+axios.defaults.headers.common['Access-Control-Allow-Methods'] = 'GET, PUT, POST, DELETE';
+axios.defaults.headers.common['Access-Control-Allow-Headers'] = 'Origin, Content-Type, X-Auth-Token';
 Vue.prototype.$http = axios;
 
 new Vue({
