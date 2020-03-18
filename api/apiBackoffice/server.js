de804e1da011842529a9065c36007609b3cbcd9c
apinot <antoine.pinot1@gmail.com>
2020-03-18 09:48:33 +0100

    add materialize

--- api/apiBackoffice/server.js
+++ api/apiBackoffice/server.js
@@ -1,6 +1,27 @@
 const express = require('express');
-// create app
+const cors = require('cors');
+const parser = require('body-parser');
+const mongoose = require('mongoose');
+
+// creation de l'application
 const app = express();
+
+app.use(cors());
+app.use(parser.json());
+
+// connexion à la base de données
+mongoose.connect("mongodb://databaseGeoQuizz/Geoquizz", {
+    useCreateIndex: true,
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+/* Routes */
+app.get('/', (req, res) => {
+    res.json({app: 'Back-office GeoQuizz'});
+});
+
+
 
 app.listen(8080, () => {
     console.log('api backoffice is running !');
