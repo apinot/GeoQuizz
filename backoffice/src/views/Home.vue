f90b24039c175b348d5e6a3dd31917852fb7fe59
felixleo22 <leofelixoff@outlook.fr>
2020-03-24 17:09:46 +0100

    ccs du menu

--- backoffice/src/views/Home.vue
+++ backoffice/src/views/Home.vue
@@ -1,64 +1,5 @@
 <template>
   <div class="home">
-    <nav>
-      <div class="nav-wrapper red darken-4">
-        <a href="/" class="brand-logo">Backoffice GeoQuizz</a>
-        <ul class="right">
-          <template v-if="isAuth">
-            <li>
-              <router-link class="btn red darken-3 waves-effect waves-light"
-              :to="{name: 'galerie'}">Gallerie</router-link>
-            </li>
-            <li>
-              <router-link class="btn red darken-3 waves-effect waves-light"
-              :to="{name: 'series'}">Series</router-link>
-            </li>
-            <li>
-              <router-link :to="{name: 'signout'}">
-                <button class="btn waves-effect waves-light">
-                  Déconnexion<i class="fas fa-sign-out-alt right" style="line-height: inherit"></i>
-                </button>
-              </router-link>
-            </li>
-          </template>
-          <template v-else>
-            <li>
-              <router-link class="btn red darken-3 waves-effect waves-light"
-              :to="{name: 'signup'}">S'inscrire</router-link>
-            </li>
-            <li>
-              <router-link class="btn red darken-3 waves-effect waves-light"
-              :to="{name: 'signin'}">
-                Se connecter</router-link>
-            </li>
-          </template>
-        </ul>
-      </div>
-    </nav>
-    <ul class="sidenav" id="mobile-demo">
-      <template v-if="isAuth">
-        <li>
-          <router-link class="btn red darken-3" :to="{name: 'galerie'}">Gallerie</router-link>
-        </li>
-        <li>
-          <router-link class="btn red darken-3" :to="{name: 'series'}">Series</router-link>
-        </li>
-        <li>
-          <router-link class="btn red darken-3" :to="{name: 'signout'}">
-            Se déconnecter<i class="fas fa-sign-out-alt"></i>
-          </router-link>
-        </li>
-      </template>
-      <template v-else>
-        <li>
-          <router-link class="btn red darken-3" :to="{name: 'signup'}">S'inscrire</router-link>
-        </li>
-        <li>
-          <router-link class="btn red darken-3" :to="{name: 'signin'}">Se connecter</router-link>
-        </li>
-      </template>
-    </ul>
-
   </div>
 </template>
 
@@ -66,18 +7,9 @@
 export default {
   name: 'Home',
   components: {},
-  computed: {
-    isAuth() {
-      return this.$store.getters.isAuth;
-    },
-  },
-  mounted() {
-    window.M.AutoInit();
-  },
 };
 </script>
 
 <style>
-
 
 </style>
